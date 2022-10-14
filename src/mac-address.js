da3932140ffa3eff14ccb578ab8a655412503546
Ihor Paiuk <ihor.paiuk@gmail.com>
2022-10-14 13:03:46 +0300

    edit: mac-address new

--- src/mac-address.js
+++ src/mac-address.js
@@ -16,18 +16,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
  let inputString = "00-1B-63-84-45-E6";
  function isMAC48Address(inputString) {
- let string = inputString.replace(/-/g, '');
-     string = string.replace(/[G-Z]/gi, '');
-   let a = parseInt(string.substr(1, 2), 16);
-   let b = parseInt(string.substr(3, 2), 16);
-   let c = parseInt(string.substr(5, 2), 16);
-   let d = parseInt(string.substr(7, 2), 16);
-   let e = parseInt(string.substr(9, 2), 16);
-   let f = parseInt(string.substr(11, 2), 16);
-   if (a <= 255 && b <= 255 && c <= 255 && d <= 255 && e <= 255 && f <= 255 && !undefined && !null && !"" && !NaN) { return true; }
-   else { return false; }
- }
- isMAC48Address(inputString);
+  let string = inputString.replace(/[-, G-Z]/gi, '');
+   if (string.length === 12) {
+  let a = parseInt(string.substr(1, 2), 16);
+  let b = parseInt(string.substr(3, 2), 16);
+  let c = parseInt(string.substr(5, 2), 16);
+  let d = parseInt(string.substr(7, 2), 16);
+  let e = parseInt(string.substr(9, 2), 16);
+  let f = parseInt(string.substr(11, 2), 16);
+if (a <= 255 && b <= 255 && c <= 255 && d <= 255 && e <= 255 && f <= 255) { return true; };
+   } else {return false;}
+}
+isMAC48Address(inputString);
 
 module.exports = {
   isMAC48Address
