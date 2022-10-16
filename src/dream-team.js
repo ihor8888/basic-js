a49d88f2cf0599fd8c5631b6c481a34b192722a9
Ihor Paiuk <ihor.paiuk@gmail.com>
2022-10-16 20:19:12 +0300

    add: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function createDreamTeam(members) {
+  if (!Array.isArray(members))
+      return false;
+
+  let chars = [];
+  members.forEach((member) => {
+      if (typeof member === 'string') {
+          chars.push( member.trim().charAt(0).toUpperCase() )
+      }
+  })
+
+  return chars.sort().join('')
 }
+
+ createDreamTeam([['David Abram'], ['Robin Attfield'], 'Thomas Berry', ['Paul R.Ehrlich'], 'donna Haraway', ' BrIaN_gOodWiN  ', { 0: 'Serenella Iovino'}, 'Erazim Kohak',
+ '  val_plumwood',  ]);
+
 
 module.exports = {
   createDreamTeam
